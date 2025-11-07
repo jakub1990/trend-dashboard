d0be7674cd52f075f46fde1cbf9bb5f687796e16
jakub1990 <33639744+jakub1990@users.noreply.github.com>
2025-11-07 16:35:22 +0100

    repair googletag

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+GA_ID = "G-LVHMK6XJJR"
+st.components.v1.html(f"""
+    <script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){{dataLayer.push(arguments);}}
+        gtag('js', new Date());
+        gtag('config', '{GA_ID}');
+    </script>
+""", height=0, width=0)
+
 st.markdown("""
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-LVHMK6XJJR"></script>
 <script>
